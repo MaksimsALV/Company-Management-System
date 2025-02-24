0923694a3b2a32d32f14157983a62fd351b8adc4
MaksimsALV <maxim.ahmetov@gmail.com>
2025-02-24 22:19:34 +0200

    Update AddEmployee.java

--- src/Commit/AddEmployee.java
+++ src/Commit/AddEmployee.java
@@ -1,27 +1,47 @@
 package Commit;
 
-import Storage.Departments;
 import Interface.Interface;
+import Storage.Departments;
+import Storage.Employees;
+import Storage.Mapping;
+
 import java.util.Scanner;
-
 //todo add validation to number imputs
-public class AddDepartment {
-    public static void addDepartment() {
-        Scanner cd = new Scanner(System.in);
+public class AddEmployee {
+    public static void addEmployee() {
+        Scanner ae = new Scanner(System.in);
 
-        System.out.println("Enter Department name");
+        //here we put employee into our employee DB
+        System.out.println("Enter Employee name");
         System.out.print("Input: ");
-        String departmentName = cd.nextLine();
+        String employeeName = ae.nextLine();
 
-        int departmentID = Departments.getDepartments().size() + 1; //start adding values from 1.
-        Departments.getDepartments().put(departmentID, departmentName);
-        System.out.println(departmentName + " is added to the Company\n");
-        System.out.println("1. Add more departments");
+        System.out.println("Enter Employee ID");
+        System.out.print("Input: ");
+        int employeeID = ae.nextInt();
+
+        Employees.getEmployees().put(employeeID, employeeName);
+
+        //here we are getting the departments list from department DB in preparation to merging within employeeDepartmentMap DB
+        System.out.println("Assign Employee to Department by typing number");
+        Departments.getDepartments().forEach((departmentID, departmentName) ->
+                System.out.println(departmentID + "." + departmentName)
+        );
+        System.out.print("Input: ");
+        int assign = ae.nextInt();
+
+        //here we are .putting our employee into employeeDepartmentMap DB by calling .assign() method from Mapping class and storing employeeID AND departmentID into selected department via int assign = ae.nextInt();
+        Mapping.assign(employeeID, assign);
+        String departmentName = Departments.getDepartments().get(assign); //retrieves department names, using assigned ID by ID understands departmentName...
+        System.out.println(employeeName + " is assigned to selected department: " + departmentName + "\n");
+
+        System.out.println("1. Add more employees");
         System.out.println("0. Return back to main menu");
         System.out.print("Input: ");
-        int choice = cd.nextInt();
+        int choice = ae.nextInt();
+        ae.nextLine();
         if (choice == 1) {
-            addDepartment();
+            addEmployee();
         }
         if (choice == 0) {
             Interface.mainMenu();
